b5da206dd8c5b66614367fc2f5a18c0d9ade81dd
Fuad Saud <fuadksd@gmail.com>
2013-04-14 03:11:18 -0300

    Add ERB template for directory listing

--- lib/http/server/content.rb
+++ lib/http/server/content.rb
@@ -1,16 +1,23 @@
+require 'erb'
+
 module HTTP
   module Server
     class Content
 
+      DIR_LISTING_TEMPLATE_PATH = File.join(File.dirname(__FILE__),
+                                            'dir_listing.erb')
+
+      DIR_LISTING_TEMPLATE = File.read(DIR_LISTING_TEMPLATE_PATH)
+
       attr_reader :data, :length
 
-      def initialize(path)
-        path = File.join(Server.dir, path)
+      def initialize(relative_path)
+        path = File.join(Server.dir, relative_path)
 
         if File.file?(path)
           @data = File.read(path)
         elsif File.directory?(path)
-          @data = Dir.entries(path).to_s
+          @data = ERB.new(DIR_LISTING_TEMPLATE).result(binding)
         end
 
         @length = @data.bytesize if @data
